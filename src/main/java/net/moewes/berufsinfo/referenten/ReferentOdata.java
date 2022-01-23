ff9684dda891314da0d250f2c787431c0224bbde
moewes <moewes@users.noreply.github.com>
2022-01-23 17:02:25 +0100

    Initial Commit

--- src/main/java/net/moewes/berufsinfo/referenten/ReferentOdata.java
+++ src/main/java/net/moewes/berufsinfo/referenten/ReferentOdata.java
@@ -1,9 +1,14 @@
 package net.moewes.berufsinfo.referenten;
 
 import lombok.Data;
+import net.moewes.quarkus.odata.annotations.EntityKey;
 import net.moewes.quarkus.odata.annotations.ODataEntity;
 
 @Data
 @ODataEntity("Referent")
 public class ReferentOdata {
+
+    @EntityKey
+    private String id;
+    private String name;
 }
